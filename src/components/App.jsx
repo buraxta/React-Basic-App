5c565f3c86c3f8c4cc8a28d2fd84c05a5eae5974
Burak Güllüler <burakgulluler@gmail.com>
2023-04-17 22:50:59 +0300

    Basic Structure

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+
+function App() {
+    return(
+        <>
+        <Heading />
+        <Footer />
+        <Note />
+        </>
+    );
+}
+
+export default App;
